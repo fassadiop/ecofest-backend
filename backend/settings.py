db832cea2d5ea6ef2d943ee573ab73597dd127dc
fassadiop <mohamadoudiop2@esp.sn>
2025-11-25 21:55:37 +0000

    Initial backend push with Django + PostgreSQL setup

--- backend/settings.py
+++ backend/settings.py
@@ -156,12 +156,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('DB_NAME'),
-    'USER': os.getenv('DB_USER'),
-    'PASSWORD': os.getenv('DB_PASSWORD'),
-    'HOST': os.getenv('DB_HOST', 'localhost'),
-    'PORT': os.getenv('DB_PORT', '5432'),
-    }
+             {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 }

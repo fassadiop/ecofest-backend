ce05d799783674530f6a00783d2af6fa8529bc15
fassadiop <mohamadoudiop2@esp.sn>
2025-11-26 21:22:24 +0000

    force redeploy

--- inscriptions/signals.py
+++ inscriptions/signals.py
@@ -1,17 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Participant
+from .models import Inscription
 from .tasks import send_invitation_package
 
-
-@receiver(post_save, sender=Participant)
+@receiver(post_save, sender=Inscription)
 def trigger_invitation_generation(sender, instance, created, **kwargs):
     """
-    Quand l'admin passe une inscription en 'VALIDÉE',
-    on déclenche automatiquement le task Celery
-    pour générer le badge + lettre PDF + envoi email.
+    Déclenche automatiquement la génération du badge + invitation PDF
+    lorsque l'INSCRIPTION passe en statut 'Validé'.
     """
 
-    # Seulement si le status devient VALIDÉ
-    if instance.statut == "VALIDEE":
+    # On ignore la création initiale (statut = En_attente)
+    if created:
+        return
+
+    # On ne réagit que lorsque le statut devient Validé
+    if instance.statut == "Validé":
         send_invitation_package.delay(instance.id)

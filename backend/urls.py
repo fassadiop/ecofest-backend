85a1e95d013e52cd9516ae947d733099a060da5e
fassadiop <mohamadoudiop2@esp.sn>
2025-11-26 17:27:23 +0000

    Edit backend/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -3,21 +3,34 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
-from inscriptions.views import AdminInscriptionListView, ResendConfirmationAPIView
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from inscriptions.views import (
+    validate_inscription,
+    refuse_inscription,
+    get_badge_url,
+    get_pieces_urls,
+    AdminInscriptionListView,
+    ResendConfirmationAPIView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # JWT endpoints (accessible at /api/token/ and /api/token/refresh/)
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Ton API - inclusion des routes définies dans inscriptions/urls.py
     path('api/', include('inscriptions.urls')),
     path('api/admin/inscriptions/', AdminInscriptionListView.as_view(), name='admin-inscriptions'),
     path("api/inscriptions/<int:pk>/resend_confirmation/", ResendConfirmationAPIView.as_view(), name="inscription-resend"),
+
+    path("api/admin/inscriptions/<int:pk>/validate/", validate_inscription),
+    path("api/admin/inscriptions/<int:pk>/refuse/", refuse_inscription),
+    path("api/admin/inscriptions/<int:pk>/badge/", get_badge_url),
+    path("api/admin/inscriptions/<int:pk>/pieces/", get_pieces_urls),
 ]
 
-# servir les médias en dev
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

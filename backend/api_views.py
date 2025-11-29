8775fdf307473ce0be8680a5a34c0215aa7595a9
fassadiop <mohamadoudiop2@esp.sn>
2025-11-29 10:15:46 +0000

    fix(api): add @api_view to me and ensure admin_users endpoint

--- backend/api_views.py
+++ backend/api_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+﻿from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
